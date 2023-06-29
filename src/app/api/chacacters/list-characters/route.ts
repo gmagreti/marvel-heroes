48471cae244f66367ab794d7b4b2a6d5c51334ae
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-06-29 18:01:36 -0300

    fix: adjustin error on build

--- src/app/api/chacacters/list-characters/route.ts
+++ src/app/api/chacacters/list-characters/route.ts
@@ -1,10 +1,9 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 import MarvelHttpClient from "~/services/MarvelHttpClient";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: any) {
   if (req.method !== "GET") {
-    return res.status(405).end();
+    return NextResponse.json({ message: "Request invalid" });
   }
 
   const httpClient = new MarvelHttpClient();
