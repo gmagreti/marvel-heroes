5c0c9ee3d5e0d7b6f20efcb2e24dfc9ba0945dc8
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-06-29 18:25:55 -0300

    testes

---
+++ src/services/testes.ts
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { api } from "~/lib/axios";
+import MarvelHttpClient from "./MarvelHttpClient";
+
+class CharactersService {
+  httpClient: MarvelHttpClient;
+
+  constructor() {
+    this.httpClient = new MarvelHttpClient();
+  }
+
+  async listCharacters(params?: any) {
+    const response = await api.get(`/chacacters/list-characters`, {
+      params,
+    });
+
+    return response;
+  }
+
+  async getCharactersById(id: string) {
+    const httpResponse = await this.httpClient.request({
+      url: `characters/${id}`,
+      method: "get",
+    });
+
+    return httpResponse;
+  }
+}
+
+export const charactersService = new CharactersService();
