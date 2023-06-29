8004752edb5b20027c86889dafe603265eb87b8b
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-06-29 12:18:19 -0300

    feat(Container): finalizing component

---
+++ src/components/layouts/container.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1250px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 16px;
+  padding-right: 16px;
+`;
