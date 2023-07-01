1c6b11c181a332414f89bdcf3253817c3ea9fba6
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-07-01 00:20:42 -0300

    feat: going through the links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { Header, NavBar } from "~/components/layouts";
+import { navLinks } from "~/mocks/nav-links";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <NavBar />
+      <NavBar navLinks={navLinks} />
     </>
   );
 }
