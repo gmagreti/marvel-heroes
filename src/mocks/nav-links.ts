0a972d3073b5096687f94de32b6a13bd4e8ee830
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-07-01 00:20:08 -0300

    feat: mock for navbar component

---
+++ src/mocks/nav-links.ts
@@ -0,0 +1,32 @@
+export const navLinks = [
+  {
+    id: "characters",
+    name: "characters",
+    href: "/characters",
+  },
+  {
+    id: "cosmics",
+    name: "cosmics",
+    href: "/cosmics",
+  },
+  {
+    id: "creators",
+    name: "creators",
+    href: "/creators",
+  },
+  {
+    id: "events",
+    name: "events",
+    href: "/events",
+  },
+  {
+    id: "series",
+    name: "series",
+    href: "/series",
+  },
+  {
+    id: "stories",
+    name: "stories",
+    href: "/stories",
+  },
+];
