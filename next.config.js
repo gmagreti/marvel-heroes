b840ab999c6cdfb387fe468ae97ac410574145d9
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-07-01 15:51:15 -0300

    feat: add hostname

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "marvel-hub.vercel.app",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
