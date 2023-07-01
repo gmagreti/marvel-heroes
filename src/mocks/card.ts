cf5007c4b38a874527a1c8be3fa049e785b6a5ec
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-07-01 15:52:14 -0300

    feat: add cards mock

---
+++ src/mocks/card.ts
@@ -0,0 +1,38 @@
+export const cardMock = {
+  title: "Spider-Man: Redemption (1996) #2",
+  description: "J. M. DeMatteis",
+  href: "/",
+  imageUrl:
+    "https://marvel-hub.vercel.app/_next/image?url=http%3A%2F%2Fi.annihil.us%2Fu%2Fprod%2Fmarvel%2Fi%2Fmg%2Fb%2Fd0%2F5a21ba7fa454e.jpg&w=384&q=75",
+};
+
+export const cardsMock = [
+  {
+    title: "Spider-Man: Redemption (1996) #2",
+    description: "J. M. DeMatteis",
+    href: "/",
+    imageUrl:
+      "https://marvel-hub.vercel.app/_next/image?url=http%3A%2F%2Fi.annihil.us%2Fu%2Fprod%2Fmarvel%2Fi%2Fmg%2Fb%2Fd0%2F5a21ba7fa454e.jpg&w=384&q=75",
+  },
+  {
+    title: "Rocket Raccoon: Marvel Tales (2023) #1",
+    description: "Unknown",
+    href: "/",
+    imageUrl:
+      "https://marvel-hub.vercel.app/_next/image?url=http%3A%2F%2Fi.annihil.us%2Fu%2Fprod%2Fmarvel%2Fi%2Fmg%2Fc%2Fa0%2F645111936022a.jpg&w=640&q=75",
+  },
+  {
+    title: "Venom: Lethal Protector II (2023) #2",
+    description: "David Michelinie",
+    href: "/",
+    imageUrl:
+      "https://marvel-hub.vercel.app/_next/image?url=http%3A%2F%2Fi.annihil.us%2Fu%2Fprod%2Fmarvel%2Fi%2Fmg%2F8%2Fb0%2F64511213bedec.jpg&w=640&q=75",
+  },
+  {
+    title: "Doctor Strange (2023) #2",
+    description: "Jed Mackay",
+    href: "/",
+    imageUrl:
+      "https://marvel-hub.vercel.app/_next/image?url=http%3A%2F%2Fi.annihil.us%2Fu%2Fprod%2Fmarvel%2Fi%2Fmg%2F0%2F03%2F645111b8a3654.jpg&w=640&q=75",
+  },
+];
