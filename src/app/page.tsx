7fd5265cf3f97bbbe44559f7f04963cf4c3958d1
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-07-01 15:55:00 -0300

    feat: add list cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import { Container } from "~/components/layouts";
+import { Container, ListCards } from "~/components/layouts";
+import { listCardsMock } from "~/mocks/list-cards";
 
 export default function Home() {
   return (
     <Container>
       <h1>The content is coming</h1>
+
+      {listCardsMock.map(({ cards, href, title }) => (
+        <ListCards key={title} cards={cards} href={href} title={title} />
+      ))}
     </Container>
   );
 }
