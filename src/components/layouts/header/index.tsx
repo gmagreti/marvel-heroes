72fb73b871f94928b4217c1fdc54d0776d4f8812
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-07-01 00:50:36 -0300

    chore: remove import

--- src/components/layouts/header/index.tsx
+++ src/components/layouts/header/index.tsx
@@ -4,7 +4,6 @@ import { LogoMarvel } from "~/components/icons";
 import * as Styled from "./styles";
 
 export function Header() {
-  const logo = <LogoMarvel />;
   return (
     <Styled.Root>
       <Container>
