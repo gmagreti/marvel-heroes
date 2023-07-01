bf31d71192ddecc5123564fc0893fea31efa8ea2
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-07-01 15:51:59 -0300

    feat: add list cards mock

---
+++ src/mocks/list-cards.ts
@@ -0,0 +1,34 @@
+import { cardsMock } from "./card";
+
+export const listCardsMock = [
+  {
+    title: "Characters",
+    href: "/characters",
+    cards: cardsMock,
+  },
+  {
+    title: "Cosmics",
+    href: "/cosmics",
+    cards: cardsMock,
+  },
+  {
+    title: "creators",
+    href: "/creators",
+    cards: cardsMock,
+  },
+  {
+    title: "events",
+    href: "/events",
+    cards: cardsMock,
+  },
+  {
+    title: "series",
+    href: "/series",
+    cards: cardsMock,
+  },
+  {
+    title: "stories",
+    href: "/stories",
+    cards: cardsMock,
+  },
+];
